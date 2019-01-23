7951f54a6ac350fc1cdd97b397df16d1d4285be8
jerry <1394367234@qq.com>
2019-01-23 14:20:16 +0800

    修改artifactId

---
+++ src/main/java/Bio.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C), 2018, Jerry
+ *
+ * @ProjectName: JavaUtil
+ * @Package: PACKAGE_NAME
+ * @ClassName: Bio
+ * @Author: jerry
+ * @Date: 2018/8/21 13:21
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+
+/**
+ * @Description:
+ * @author jerry
+ * @createDateTime 2018/12/29 14:45
+ */
+public class Bio {
+
+
+}
